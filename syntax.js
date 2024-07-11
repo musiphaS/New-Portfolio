96bc9836adf614f599f1a485e298a86311afdb84
Kelvin <kelvinmusipha@gmail.com>
2024-07-11 04:26:45 -0700

    first project

---
+++ syntax.js
@@ -0,0 +1,8 @@
+// according to the code below we are targeting the 2 consts (or elements)
+// and whenever we click it its gonna add and remove the open class 
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+}
