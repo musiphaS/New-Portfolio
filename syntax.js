7e037ea95179aa45394a6ac132b6e25c6a9d7102
Kelvin <kelvinmusipha@gmail.com>
2024-07-14 18:30:50 -0700

    jjfhy

--- syntax.js
+++ syntax.js
@@ -1,8 +1,16 @@
 // according to the code below we are targeting the 2 consts (or elements)
 // and whenever we click it its gonna add and remove the open class 
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle('open')
-    icon.classList.toggle('open')
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle('open')
+//     icon.classList.toggle('open')
+// }
+
+function toggleMobileMenu() {
+    menu.classList.toggle('open');
+  }
+  
+ function toggleMobileMenu() {
+  menu.classList.toggle('open');
 }
